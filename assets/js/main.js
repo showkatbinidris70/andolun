1991257cac5614bc731d5b922b438138d42cc354
showkatbinidris70 <showkatbinidris70@gmail.com>
2024-09-01 16:28:57 +0600

    font

--- assets/js/main.js
+++ assets/js/main.js
@@ -364,7 +364,7 @@
   });
 
   // upcoming event start
-  var countdownDate = new Date("Dec 31, 2024 15:00:00").getTime();
+  var countdownDate = new Date("Dec 31, 2024 10:00:00").getTime();
 
   // Update the countdown every 1 second
   var x = setInterval(function () {
@@ -374,7 +374,7 @@
     // Calculate days, hours, minutes, and seconds
     var days = Math.floor(distance / (1000 * 60 * 60 * 24));
     var hours = Math.floor(
-      (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+      (distance % (1000 * 6 * 60 * 24)) / (1000 * 60 * 60)
     );
     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
