e02316f19a550fe99564b78bbe3853093ca82acb
showkatbinidris70 <showkatbinidris70@gmail.com>
2024-09-01 13:35:05 +0600

    event

--- assets/js/main.js
+++ assets/js/main.js
@@ -362,6 +362,39 @@
     mousewheel: true,
     keyboard: true,
   });
+
+  // upcoming event start
+  var countdownDate = new Date("Dec 31, 2024 15:00:00").getTime();
+
+  // Update the countdown every 1 second
+  var x = setInterval(function () {
+    var now = new Date().getTime();
+    var distance = countdownDate - now;
+
+    // Calculate days, hours, minutes, and seconds
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor(
+      (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+    );
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    // Display the result
+    document.getElementById("days").innerHTML = days;
+    document.getElementById("hours").innerHTML = hours;
+    document.getElementById("minutes").innerHTML = minutes;
+    document.getElementById("seconds").innerHTML = seconds;
+
+    // If the countdown is finished
+    if (distance < 0) {
+      clearInterval(x);
+      document.getElementById("days").innerHTML = "0";
+      document.getElementById("hours").innerHTML = "0";
+      document.getElementById("minutes").innerHTML = "0";
+      document.getElementById("seconds").innerHTML = "0";
+    }
+  }, 1000);
+  // upcoming event end
 })();
 
 // ---------------------------------------------------------------------
